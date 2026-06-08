1715b328338a5aeabc5b0892fd2322ad29c5c28f
rajkumar <rema1191@gmail.com>
2026-06-08 22:00:44 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-app');
+
+  name = "Rajkumar";
+
+  inputValue = '';
+
+  imgSrc = "https://png.pngtree.com/thumb_back/fh260/background/20230411/pngtree-nature-forest-sun-ecology-image_2256183.jpg"
+
+  handleClick() {
+    alert('Button clicked!');
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,19 @@ import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+
+  /*
+     Components - the building blocks of Angular applications. They control a patch of screen called a view.
+
+     {{}} - interpolation syntax to display data from the component class in the template.
+
+     Data binding - the mechanism for coordinating parts of a template with parts of a component. It can be one-way (interpolation, property binding, event binding) or two-way (two-way data binding).
+
+     event binding - a way to listen for and respond to user actions in the template. It uses parentheses () to bind an event to a method in the component class.
+
+     property binding - a way to bind a property of an HTML element to a property in the component class. It uses square brackets [src]="imgSrc" to bind a property.
+
+     [()] - two-way data binding syntax that combines property binding and event binding. It allows for automatic synchronization of data between the component class and the template.
+
+  */
